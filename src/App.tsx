6d184b026597679f0a4aae39c16ed95f8f7f7a75
Alang Kim <rlaangh77@naver.com>
2022-08-18 01:17:33 +0900

    feat: 새로고침 버튼 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import { ContractList } from "./contracts/components/Contract.view";
 import useContracts from "./contracts/hooks/use-contracts.hook";
 
 function App() {
-  const [contracts] = useContracts();
+  const [contracts, fetchContracts] = useContracts();
 
   return (
     <div className="App">
+      <button onClick={() => fetchContracts()}>새로 고침</button>
       <ContractList contracts={contracts} />
     </div>
   );
