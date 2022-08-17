fdb84c629dc466c130d9f5c3e4ef56ef27acdf3a
Alang Kim <rlaangh77@naver.com>
2022-08-18 01:39:21 +0900

    feat: user mock api 추가

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,8 +1,12 @@
 import { rest } from "msw";
 import contractsMock from "./__mocks__/contracts.json";
-
+import usersMock from "./__mocks__/users.json";
 export const handlers = [
   rest.get("/contract", (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(contractsMock));
+  }),
+
+  rest.get("/user", (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(usersMock));
   }),
 ];
